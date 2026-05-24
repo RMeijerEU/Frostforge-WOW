abfa9c77074ad68e0b1f90f165968616d80f4341
Ramon Meijer <ramon@rmeijer.eu>
2026-05-24 16:08:07 +0200

    Fix Uldaman creature template rank column

--- sql/013_frostforge_uldaman.sql
+++ sql/013_frostforge_uldaman.sql
@@ -27,7 +27,7 @@ DELETE FROM creature_template WHERE entry BETWEEN 900060 AND 900069;
 INSERT INTO creature_template
 (entry, difficulty_entry_1, difficulty_entry_2, difficulty_entry_3, KillCredit1, KillCredit2, name, subname, IconName, gossip_menu_id,
  minlevel, maxlevel, exp, faction, npcflag, speed_walk, speed_run, speed_swim, speed_flight, detection_range,
- rank, dmgschool, DamageModifier, BaseAttackTime, RangeAttackTime, BaseVariance, RangeVariance, unit_class, unit_flags,
+ `rank`, dmgschool, DamageModifier, BaseAttackTime, RangeAttackTime, BaseVariance, RangeVariance, unit_class, unit_flags,
  unit_flags2, dynamicflags, family, type, type_flags, lootid, pickpocketloot, skinloot, PetSpellDataId, VehicleId,
  mingold, maxgold, AIName, MovementType, HoverHeight, HealthModifier, ManaModifier, ArmorModifier, ExperienceModifier,
  RacialLeader, movementId, RegenHealth, CreatureImmunitiesId, flags_extra, ScriptName, VerifiedBuild)
